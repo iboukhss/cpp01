3638cbb59777fdb13b7c18c83e4afd5c6949659d
Ismail <151830328+iboukhss@users.noreply.github.com>
2025-07-18 13:34:05 +0200

    formatting

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -8,13 +8,15 @@ int main()
     std::string* stringPTR = &str;
     std::string& stringREF = str;
 
-    std::cout << "Memory address of string:                " << &str << '\n';
-    std::cout << "Memory address held by string pointer  : " << stringPTR << '\n';
-    std::cout << "Memory address held by string reference: " << &stringREF << '\n';
+    std::cout << "Memory address of string:                " << &str << "\n";
+    std::cout << "Memory address held by string pointer:   " << stringPTR << "\n";
+    std::cout << "Memory address held by string reference: " << &stringREF << "\n";
 
-    std::cout << "Value of string:            " << str << '\n';
-    std::cout << "Value pointed by stringPTR: " << *stringPTR << '\n';
-    std::cout << "Value pointed by stringREF: " << stringREF << '\n';
+    std::cout << "\n";
+
+    std::cout << "Value of string:                         " << str << "\n";
+    std::cout << "Value pointed by stringPTR:              " << *stringPTR << "\n";
+    std::cout << "Value pointed by stringREF:              " << stringREF << "\n";
 
     return 0;
 }
